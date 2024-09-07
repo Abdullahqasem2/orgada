587ddf99cad0648344792ce6fe194d6b3a72db70
Abdullahqasem2 <abdullahzq97@gmail.com>
2024-09-07 14:35:43 +0300

    first commit

---
+++ src/components/features.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Features = (props) => {
+  return (
+        <img src={"../../img/comming.png"} alt="Feature Image" style={{ width: "100%" , height: "100%" }} />
+
+  );
+};
